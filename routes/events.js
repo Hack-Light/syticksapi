36c71d48e8d73f3dc5e9603fc574f59182df1ae9
Anthony Okpako <melowsworld@yahoo.co.uk>
2021-11-24 21:28:51 +0100

    category

--- routes/events.js
+++ routes/events.js
@@ -1,25 +1,26 @@
-const express = require("express"),
-  {
-    getAllEvent,
-    createComments,
-    getEventComment,
-    createReply,
-    getReplyComment,
-    deleteComments
-  } = require("../controller/event");
+const express = require('express'),
+	{
+		getAllEvent,
+		createComments,
+		getEventComment,
+		createReply,
+		getReplyComment,
+		deleteComments,
+	} = require('../controller/event');
 
 let router = express.Router();
 
-router.get("/events", getAllEvent);
+router.get('/events', getAllEvent);
 
-router.post("/event/comment", getEventComment);
+router.post('/event/comment', getEventComment);
 
-router.post("/event/comment/create", createComments);
+router.post('/event/comment/create', createComments);
 
-router.post("/event/reply/create", createReply);
+router.post('/event/reply/create', createReply);
 
-router.post("/event/reply", getReplyComment);
+router.post('/event/reply', getReplyComment);
 
-router.post("/event/comment/delete", deleteComments);
+router.post('/event/comment/delete', deleteComments);
+router.post('/event/category', getCategory);
 
 module.exports = router;
