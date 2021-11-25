ec67c9db3cd6040af13a06ffd8ce3266c6e2c909
Anthony Okpako <melowsworld@yahoo.co.uk>
2021-11-25 03:48:06 +0100

    category

--- controller/event.js
+++ controller/event.js
@@ -339,13 +339,13 @@ exports.getCategory = async (req, res, next) => {
 					},
 				],
 			});
+		console.log(events);
 
 		return res.status(200).json({
 			success: true,
-			events: events,
+			data: events,
 		});
 	} catch (error) {
-		console.log(error);
 		return res.status(500).json({
 			success: false,
 			message: error.message,
