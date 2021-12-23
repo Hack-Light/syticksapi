1a261eb03647944a26ed9498490ae8a562c07761
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-12-23 02:22:01 +0100

    worked on login and register

--- controller/login.js
+++ controller/login.js
@@ -1,64 +1,67 @@
-const jwt = require("jsonwebtoken"),
-  bCrypt = require("bcrypt"),
-  userModel = require("../models/user");
+const jwt = require('jsonwebtoken'),
+	bCrypt = require('bcrypt'),
+	userModel = require('../models/user');
 
 exports.loginUser = async (req, res, next) => {
-  const { password, email } = req.body;
-  try {
-    let user = await userModel.findOne({ email: email });
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User does not exist",
-        error: {
-          statusCode: 404,
-          description: "User does not exist"
-        }
-      });
-    } else {
-      let result = await bCrypt.compare(password, user.password);
-      if (!result) {
-        return res.status(404).json({
-          success: false,
-          message: "Incorrect password",
-          error: {
-            statusCode: 404,
-            description: "Incorrect password"
-          }
-        });
-      }
-      let api_token = jwt.sign(
-        {
-          phone_number: user.phone,
-          role: user.role
-        },
-        process.env.JWT_KEY
-      );
-      user.api_token = api_token;
+	const { password, email, name, username } = req.body;
+	try {
+		let user = await userModel.findOne({
+			$or: [
+				{ email: { $regex: email, $options: 'i' } },
+				{ username: { $regex: username, $options: 'i' } },
+			],
+		});
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'User does not exist',
+				error: {
+					statusCode: 404,
+					description: 'User does not exist',
+				},
+			});
+		} else {
+			let result = await bCrypt.compare(password, user.password);
+			if (!result) {
+				return res.status(404).json({
+					success: false,
+					message: 'Incorrect password',
+					error: {
+						statusCode: 404,
+						description: 'Incorrect password',
+					},
+				});
+			}
+			let api_token = jwt.sign(
+				{
+					phone_number: user.phone,
+					role: user.role,
+				},
+				process.env.JWT_KEY,
+			);
+			user.api_token = api_token;
 
-      user = await user.save();
+			user = await user.save();
 
-      return res.status(200).json({
-        success: true,
-        message: "You're logged in successfully.",
-        id: user._id,
-        phone: user.phone,
-        email: user.email,
-        username: user.username,
-        name: user.name,
-        day: user.day,
-        month: user.month,
-        year: user.year
-      });
-    }
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.message,
-      error: {
-        statusCode: 500,
-        description: err.message
-      }
-    });
-  }
+			return res.status(200).json({
+				success: true,
+				message: "You're logged in successfully.",
+				id: user._id,
+				phone: user.phone,
+				email: user.email,
+				username: user.username,
+				name: user.name,
+				api_token: user.api_token,
+			});
+		}
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+			error: {
+				statusCode: 500,
+				description: err.message,
+			},
+		});
+	}
 };
--- controller/register.js
+++ controller/register.js
@@ -1,75 +1,77 @@
-const jwt = require("jsonwebtoken"),
-  bcryptjs = require("bcrypt"),
-  User = require("../models/user"),
-  { signToken } = require("../auth/auth");
+const jwt = require('jsonwebtoken'),
+	bcryptjs = require('bcrypt'),
+	User = require('../models/user'),
+	{ signToken } = require('../auth/auth');
 
 exports.registerUser = async (req, res, next) => {
-  let { phone, password, role, username, name, email, day, month, year } =
-    req.body;
-  console.log(req.body);
+	let { phone, password, role, username, name, email, day, month, year } =
+		req.body;
+	console.log(req.body);
 
-  try {
-    let user = await User.findOne({ email: email });
-    if (user) {
-      return res.status(409).json({
-        success: false,
-        message: "User already exists",
-        error: {
-          statusCode: 409,
-          description:
-            "Phone number already taken, please use another phone number"
-        }
-      });
-    }
-    let hashpassword = await bcryptjs.hash(password, 12);
-    let user_role = role || "user";
-    let api_token = jwt.sign(
-      {
-        phone_number: phone,
-        role: user_role,
-        email,
-        username
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: "24h"
-      }
-    );
+	try {
+		let user = await User.findOne({
+			$or: [
+				{ email: { $regex: email, $options: 'i' } },
+				{ username: { $regex: username, $options: 'i' } },
+			],
+		});
+		if (user) {
+			return res.status(409).json({
+				success: false,
+				message: 'User already exists',
+				error: {
+					statusCode: 409,
+					description: 'User already taken, please check your details.',
+				},
+			});
+		}
+		let hashpassword = await bcryptjs.hash(password, 12);
+		let user_role = role || 'user';
+		let api_token = jwt.sign(
+			{
+				phone_number: phone,
+				role: user_role,
+				email,
+				username,
+			},
+			process.env.JWT_KEY,
+			{
+				expiresIn: '24h',
+			},
+		);
 
-    user = new User({
-      phone: phone,
-      password: hashpassword,
-      api_token: api_token,
-      role: user_role,
-      email,
-      username,
-      name,
-      day,
-      month,
-      year
-    });
-    user = await user.save();
+		user = new User({
+			phone: phone,
+			password: hashpassword,
+			api_token: api_token,
+			role: user_role,
+			email,
+			username,
+			name,
+			day,
+			month,
+			year,
+		});
+		user = await user.save();
 
-    return res.status(201).json({
-      success: true,
-      message: "User registration successfull",
-      id: user._id,
-      phone: user.phone,
-      email: user.email,
-      username: user.username,
-      name: user.name,
-      day: user.day,
-      month: user.month,
-      year: user.year
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.message,
-      error: {
-        statusCode: 500,
-        description: err.message
-      }
-    });
-  }
+		return res.status(201).json({
+			success: true,
+			message: 'User registration successfully',
+			id: user._id,
+			phone: user.phone,
+			email: user.email,
+			username: user.username,
+			name: user.name,
+			api_key: user.api_token,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+			error: {
+				statusCode: 500,
+				description: err.message,
+			},
+		});
+	}
 };
